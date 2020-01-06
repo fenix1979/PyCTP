677d84c2361e5890eb38dea01ecd153ae6a30789
x <x@rh>
2020-01-06 12:33:49 +0800

    PyCTP

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ Created on Sat Jul 30 14:39:27 2016
 """
 
 import platform
-from distutils.core import setup, Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from os.path import join, split, dirname
 
-headers = ['stdafx.h', 'UserApiDataType.h', 'UserApiStruct.h', 'MdApi.h', 'TraderApi.h', 'test_PyCTP.py']
+headers = ['stdafx.h', 'UserApiDataType.h', 'UserApiStruct.h', 'MdApi.h', 'TraderApi.h']
 sources = ['stdafx.cpp', 'PyCTP.cpp', 'UserApiDataType.cpp', 'UserApiStruct.cpp', 'MdApi.cpp', 'TraderApi.cpp']
 
 sources = [join('.', 'src', file) for file in sources]
@@ -19,25 +20,25 @@ optional = {}
 if platform.system() == 'Linux':
     optional['extra_compile_args'] = ['-std=c++11']
     optional['runtime_library_dirs'] = ['./']
-    optional['include_dirs']=['./v6.3.6_20160606_api_tradeapi_linux64']
-    optional['library_dirs']=['./v6.3.6_20160606_api_tradeapi_linux64']
-#    depend_dynamics = ['libthostmduserapi.so', 'libthosttraderapi.so']
+    optional['include_dirs']=['./v6.3.15_20190220_api_tradeapi_se_linux64']
+    optional['library_dirs']=['./v6.3.15_20190220_api_tradeapi_se_linux64']
 if platform.system() == 'Windows':
-    optional['include_dirs'] = ['./v6.3.6_20160606_tradeapi_windows']
-    optional['library_dirs'] = ['./v6.3.6_20160606_tradeapi_windows']
     if '64 bit' in platform.python_compiler():
-        optional['include_dirs'] = ['./v6.3.15_20190220_tradeapi64_se_windows','v6.3.15_20190220_clientdll64_windows']
-        optional['library_dirs'] = ['./v6.3.15_20190220_tradeapi64_se_windows','v6.3.15_20190220_clientdll64_windows']
-#    depend_dynamics = ['thostmduserapi.dll', 'thosttraderapi.dll']
+        optional['include_dirs'] = ['./v6.3.15_20190220_tradeapi64_se_windows']
+        optional['library_dirs'] = ['./v6.3.15_20190220_tradeapi64_se_windows']
+    else:
+        optional['include_dirs'] = ['./v6.3.15_20190220_tradeapi_se_windows']
+        optional['library_dirs'] = ['./v6.3.15_20190220_tradeapi_se_windows']
 argments = dict(name='PyCTP',
                 sources=sources,
                 language='c++',
-                libraries=['thostmduserapi_se', 'thosttraderapi_se','WinDataCollect'],
+                libraries=['thostmduserapi_se', 'thosttraderapi_se'],
+                extra_link_args=['-Wl,-rpath,/usr/local/lib/CTP'],
                 depends=depends)
 argments.update(optional)
 
 setup(name='PyCTP',
-      version='1.1.0',
+      version='1.1.1',
       description='CTP for Python',
       long_description='CTP v6.3.15_20190220 for Python',
       author='Shi Zhuolin',
@@ -46,10 +47,5 @@ setup(name='PyCTP',
       keywords=['ctp','futures','stock'],
       license='LGPL-3.0',
       platforms=['linux-x86_64','win32','win-amd64'],
-      ext_modules=[Extension(**argments)],
-      scripts=['./src/test_PyCTP.py'],
-#      #data_files=[('lib64', depend_dynamics)],
-#      #packages=['PyCTP'],
-#      #package_dir={'PyCTP':optional['library_dirs'][0]},
-#      #package_data={'PyCTP':depend_dynamics}
+      ext_modules=[Extension(**argments)]
       )
